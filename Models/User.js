e025aaf7e58dcc8426614cf185c6826389643aad
darshan <darshpat01@gmail.com>
2023-03-22 23:32:52 +0530

    auth added

---
+++ Models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = bcrypt.hash(password, salt);
+  return hash;
+};
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
+
+module.exports = mongoose.model("User", UserSchema);
--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const User = require("./Models/User");
+const jwt = require("jsonwebtoken");
+
+require("./passport");
+
 const app = express();
 const port = 3009;
+
+genToken = (user) => {
+  return jwt.sign(
+    {
+      iss: "Darshan",
+      sub: user.id,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1),
+    },
+    process.env.SECRET
+  );
+};
+
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv/config");
@@ -20,5 +40,52 @@ db.once("open", () => {
 });
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", async function (req, res, next) {
+  const { name, email, password } = req.body;
+
+  //Check If User Exists
+  let foundUser = await User.findOne({ email });
+  if (foundUser) {
+    return res.status(403).json({ error: "Email is already in use" });
+  }
+
+  const newUser = new User({ name, email, password });
+  //   encrypt password
+  newUser.password = await newUser.encryptPassword(password);
+
+  await newUser.save();
+  // Generate JWT token
+  const token = genToken(newUser);
+  res.status(200).json({ token, user: newUser });
+});
+
+app.post("/login", async function (req, res, next) {
+  const { email, password } = req.body;
+
+  //Check If User Exists
+  let foundUser = await User.findOne({ email });
+  if (!foundUser) {
+    return res.status(403).json({ error: "Invalid Credentials" });
+  }
+
+  // Check if password is correct
+  const isMatch = await foundUser.isValidPassword(password);
+  if (!isMatch) {
+    return res.status(403).json({ error: "Invalid Credentials" });
+  }
+
+  // Generate JWT token
+  const token = genToken(foundUser);
+  res.status(200).json({ token, user: foundUser });
+});
+
+app.get(
+  "/secret",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    res.json("Secret Data");
+  }
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
