0b65d16bdfdb0a1e36616797d7c91065ac90e0e0
darshan <darshpat01@gmail.com>
2023-03-22 19:57:48 +0530

    db connected

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3009;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv/config");
+}
+
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
